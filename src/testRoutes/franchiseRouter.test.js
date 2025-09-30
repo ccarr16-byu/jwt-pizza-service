8b85e0cd56b2a508e294dd442298192683189ad5
Chance Carr <ccarr16@byu.edu>
2025-09-30 15:38:28 -0600

    add franchiseRouter get all franchises test; 71.9%

---
+++ src/testRoutes/franchiseRouter.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const app = require('../service');
+const { Role, DB } = require('../database/database.js');
+
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+test('get all franchises', async () => {
+    const getAllRes = await request(app).get('/api/franchise?page=0&limit=10&name=*');
+    expect(getAllRes.status).toBe(200);
+});
