d3a14d31870ea5abe14549245987258c3ffc750f
Chance Carr <ccarr16@byu.edu>
2025-09-30 14:54:26 -0600

    Add userRouter update user test; 53.71%

--- src/testRoutes/userRouter.test.js
+++ src/testRoutes/userRouter.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const app = require('../service');
+const { Role, DB } = require('../database/database.js');
+
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
@@ -15,6 +17,19 @@ function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
 
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
 test('get authenticated user', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   const token = loginRes.body.token;
@@ -22,4 +37,21 @@ test('get authenticated user', async () => {
   const getUserRes = await request(app).get('/api/user/me').set('Authorization', `Bearer ${token}`);
   expect(getUserRes.status).toBe(200);
   expect(getUserRes.body).toMatchObject(loginRes.body.user);
+});
+
+test('update user', async () => {
+  const adminUser = await createAdminUser();
+  const loginRes = await request(app).put('/api/auth').send(adminUser);
+  const token = loginRes.body.token;
+
+  const updatedUserObject = { name: 'Updated Name', email: `${adminUser.email}`, password: `${adminUser.password}`};
+
+  const updateUserRes = await request(app).put(`/api/user/${adminUser.id}`).set('Authorization', `Bearer ${token}`).set('Content-Type', 'application/json').send(updatedUserObject);
+  expect(updateUserRes.status).toBe(200);
+  expect(adminUser.name).not.toBe(updateUserRes.body.name);
+
+  const newToken = updateUserRes.body.token;
+
+  const getUserRes = await request(app).get('/api/user/me').set('Authorization', `Bearer ${newToken}`);
+  expect(getUserRes.body.name).toBe("Updated Name");
 });
