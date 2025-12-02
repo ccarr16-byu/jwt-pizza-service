c4fcc2063372050396a42ec7cd48a403ea9afd35
Chance Carr <ccarr16@byu.edu>
2025-12-02 09:25:51 -0700

    Fix error serializing

--- src/service.js
+++ src/service.js
@@ -60,7 +60,7 @@ app.use('*', (req, res) => {
 
 // Default error handler for all exceptions and errors.
 app.use((err, req, res, next) => {
-  logger.log(logger.statusToLogLevel(err.statusCode), 'uncaughtError', {res: err.message});
+  logger.log(logger.statusToLogLevel(err.statusCode), 'uncaughtError', {res: {message: err.message, stack: err.stack}});
   res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
   next();
 });
